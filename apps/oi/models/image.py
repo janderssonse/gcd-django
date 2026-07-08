55f6b440e2eb7629ea6195a7aba9ff5d91c8f7f3
Josef Andersson <janderssonse@proton.me>
2026-07-08 15:01:07 +0200

    refactor(oi): extract image revision cluster (C1)
    Move ImageRevisionManager, _clear_image_cache and ImageRevision into
    models/image.py, re-exported. No cross-cluster dependencies.
    
    Signed-off-by: Josef Andersson <janderssonse@proton.me>

---
+++ apps/oi/models/image.py
@@ -0,0 +1,219 @@
+"""Image revision cluster (roadmap C1). Base classes and helpers come from apps.oi.models.base; re-exported through the package __init__ so the historical import surface is unchanged."""
+
+import itertools
+import operator
+import re
+import calendar
+import os
+import glob
+from datetime import timedelta
+
+from django import forms
+from django.conf import settings
+from django.db import models, transaction, IntegrityError
+from django.db.models import F, Manager
+from django.db.models.fields import Field, related
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, \
+                                               GenericRelation
+from django.utils import timezone
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape as esc
+from django.core.validators import RegexValidator, URLValidator
+from django.core.exceptions import ValidationError, FieldDoesNotExist
+
+from imagekit.cachefiles.backends import CacheFileState
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFit
+from taggit.managers import TaggableManager
+
+from functools import reduce
+
+from apps.oi import states, relpath
+
+from apps.stddata.models import Country, Language, Date, Script
+from apps.stats.models import RecentIndexedIssue, CountStats
+
+from apps.gcd.models import (
+    Publisher, IndiciaPublisher, BrandGroup, Brand, BrandUse, Series,
+    SeriesBond, Cover, Image, Issue, IssueCredit, PublisherCodeNumber,
+    CodeNumberType, Story, StoryCredit, StoryCharacter, CharacterRole,
+    StoryGroup, StoryArc, StoryArcRelation, Universe, Multiverse,
+    CharacterOrderType, CharacterOrder,
+    BiblioEntry, Reprint,
+    SeriesPublicationType, SeriesBondType, StoryType, CreditType, FeatureType,
+    Feature, FeatureLogo, FeatureRelation, Character, CharacterRelation,
+    CharacterNameDetail, Group, GroupNameDetail, GroupRelation,
+    GroupMembership, ImageType, Printer, IndiciaPrinter,
+    Creator, CreatorArtInfluence, CreatorDegree, CreatorMembership,
+    CreatorNameDetail, CreatorNonComicWork, CreatorSchool, CreatorRelation,
+    CreatorSignature, NonComicWorkYear, Award, ReceivedAward, DataSource,
+    ExternalLink, STORY_TYPES, CREDIT_TYPES, VCS_Codes)
+
+from apps.gcd.models.gcddata import GcdData, GcdLink
+
+from apps.gcd.models.issue import issue_descriptor
+from apps.gcd.models.story import show_feature, show_feature_as_text, \
+                                  show_characters, show_title, \
+                                  _get_civilian_identity
+from apps.gcd.models.image import CropToFace
+from apps.indexer.views import ErrorWithMessage
+
+from apps.oi.models.base import (
+    ChangeType, CTYPES, CTYPES_INLINE, CTYPES_BULK,
+    ACTION_ADD, ACTION_DELETE, ACTION_MODIFY,
+    IMP_BONUS_ADD, IMP_COVER_VALUE, IMP_IMAGE_VALUE,
+    IMP_APPROVER_VALUE, IMP_DELETE,
+    update_count, set_series_first_last, validated_isbn,
+    remove_leading_article, on_sale_date_as_string, on_sale_date_fields,
+    get_keywords, save_keywords, _check_year, _imps_for_years,
+    _get_revision_lock, _free_revision_lock,
+    _removed_related_objects, _process_formset,
+    Changeset, ChangesetComment, RevisionLock, RevisionManager,
+    RevisionQuerySet, Revision, OngoingReservation, ExternalLinkRevision)
+
+MONTH_CHOICES = [(i, calendar.month_name[i]) for i in range(1, 13)]
+
+
+class ImageRevisionManager(RevisionManager):
+
+    def clone_revision(self, image, changeset):
+        """
+        Given an existing Image instance, create a new revision based on it.
+
+        This new revision will be where the replacement is stored.
+        """
+        return RevisionManager._clone_revision(self,
+                                               instance=image,
+                                               instance_class=Image,
+                                               changeset=changeset)
+
+    def _do_create_revision(self, image, changeset, **ignore):
+        """
+        Helper delegate to do the class-specific work of clone_revision.
+        """
+        revision = ImageRevision(
+            # revision-specific fields:
+            image=image,
+            changeset=changeset,
+
+            # copied fields:
+            content_type=image.content_type,
+            object_id=image.object_id,
+            type=image.type)
+
+        revision.save()
+        return revision
+
+
+def _clear_image_cache(cached_image):
+    cached_image.storage.delete(cached_image.path)
+    cached_image.cachefile_backend.set_state(cached_image,
+                                             CacheFileState.DOES_NOT_EXIST)
+
+
+class ImageRevision(Revision):
+    class Meta:
+        db_table = 'oi_image_revision'
+        ordering = ['created', '-id']
+
+    objects = ImageRevisionManager()
+
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True,
+                              related_name='revisions')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     null=True)
+    object_id = models.PositiveIntegerField(db_index=True, null=True)
+    object = GenericForeignKey('content_type', 'object_id')
+
+    type = models.ForeignKey(ImageType, on_delete=models.CASCADE)
+
+    image_file = models.ImageField(upload_to='%s/%%m_%%Y' %
+                                             settings.NEW_GENERIC_IMAGE_DIR)
+    scaled_image = ImageSpecField([ResizeToFit(width=400)],
+                                  source='image_file',
+                                  format='JPEG', options={'quality': 90})
+    cropped_face = ImageSpecField([CropToFace(), ],
+                                  source='image_file',
+                                  format='JPEG',
+                                  options={'quality': 90})
+
+    marked = models.BooleanField(default=False)
+    is_replacement = models.BooleanField(default=False)
+
+    def description(self):
+        return '%s for %s' % (self.type.description,
+                              str(self.object.full_name()))
+
+    def _get_source(self):
+        return self.image
+
+    def _get_source_name(self):
+        return 'image'
+
+    def _get_blank_values(self):
+        """
+        Images don't do field comparisons, so just return an empty
+        dictionary so we don't throw an exception if code calls this.
+        """
+        return {}
+
+    def calculate_imps(self, prev_rev=None):
+        return IMP_IMAGE_VALUE
+
+    def _imps_for(self, field_name):
+        """
+        Images are done purely on a flat point model and don't really have
+        fields.  We shouldn't get here, but just in case, return 0 to be safe.
+        """
+        return 0
+
+    def commit_to_display(self):
+        image = self.image
+        if self.is_replacement:
+            prev_rev = self.previous()
+            # copy replaced image back to revision
+            prev_rev.image_file.save(str(prev_rev.id) + '.jpg',
+                                     content=image.image_file)
+            image.image_file.delete()
+        elif self.deleted:
+            image.delete()
+            return
+        elif image is None:
+            if self.type.unique and not self.is_replacement:
+                if Image.objects.filter(
+                        content_type=ContentType.objects
+                                                .get_for_model(self.object),
+                        object_id=self.object.id,
+                        type=self.type,
+                        deleted=False).count():
+                    raise ErrorWithMessage(
+                          '%s has an %s. Additional images cannot be uploaded,'
+                          ' only replacements are possible.' %
+                          (self.object, self.type.description))
+
+            # first generate instance
+            image = Image(content_type=self.content_type,
+                          object_id=self.object_id,
+                          type=self.type,
+                          marked=self.marked)
+            image.save()
+
+        # then add the uploaded file
+        image.image_file.save(str(image.id) + '.jpg', content=self.image_file)
+        self.image_file.delete()
+        self.image = image
+        if self.is_replacement:
+            _clear_image_cache(image.scaled_image)
+            _clear_image_cache(image.thumbnail)
+            _clear_image_cache(image.icon)
+            _clear_image_cache(image.cropped_face)
+        self.save()
+
+    def __str__(self):
+        return '%s for %s' % (self.type.description.capitalize(),
+                              str(self.object))
+
+
